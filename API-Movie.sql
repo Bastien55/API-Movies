df20b4b768478d9e6dc6ea7b66bc9e70f4605156
Bastien <bureaubastien3@gmail.com>
2023-10-29 00:04:14 +0200

    Changes: Maj du SQL pour les données

--- API-Movie.sql
+++ API-Movie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db
--- Généré le : mar. 24 oct. 2023 à 18:11
+-- Généré le : sam. 28 oct. 2023 à 22:03
 -- Version du serveur : 11.1.2-MariaDB-1:11.1.2+maria~ubu2204
 -- Version de PHP : 8.2.11
 
@@ -32,6 +32,20 @@ CREATE TABLE `acteurs` (
   `personne_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `acteurs`
+--
+
+INSERT INTO `acteurs` (`acteur_id`, `personne_id`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(9, 6),
+(5, 8),
+(6, 10),
+(7, 12),
+(8, 13);
+
 -- --------------------------------------------------------
 
 --
@@ -43,6 +57,17 @@ CREATE TABLE `cast` (
   `acteur_id` int(11) DEFAULT NULL,
   `realisateur_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `cast`
+--
+
+INSERT INTO `cast` (`cast_id`, `acteur_id`, `realisateur_id`) VALUES
+(2, 1, NULL),
+(3, 2, 1),
+(4, 5, 2),
+(5, 6, 3),
+(6, 7, 2);
 
 -- --------------------------------------------------------
 
@@ -63,30 +88,11 @@ CREATE TABLE `film` (
 --
 
 INSERT INTO `film` (`film_id`, `Nom`, `Description`, `Date_de_parution`, `cast_id`) VALUES
-(1, 'Star Wars III', 'An interstellar war with cool knights', '1985-07-23', NULL),
-(2, '3 jours max', 'trois jours max', '2023-10-25', NULL);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `film_personne`
---
-
-CREATE TABLE `film_personne` (
-  `ID` int(11) NOT NULL,
-  `personne_id` int(11) NOT NULL,
-  `film_id` int(11) NOT NULL,
-  `role` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `film_personne`
---
-
-INSERT INTO `film_personne` (`ID`, `personne_id`, `film_id`, `role`) VALUES
-(1, 1, 1, 'Acteur'),
-(2, 2, 2, 'Acteur'),
-(3, 2, 2, 'Réa');
+(1, 'Star Wars III', 'An interstellar war with cool knights', '1985-07-23', 2),
+(2, '3 jours max', 'trois jours max', '2023-10-25', 3),
+(3, 'Inception', 'Un thriller de science-fiction sur les rêves.', '2010-07-16', 4),
+(4, 'Fight Club', 'Un film culte sur la lutte contre la société de consommation.', '1999-10-15', 5),
+(5, 'Interstellar', 'Un voyage spatial épique pour sauver l\'humanité.', '2014-11-05', 6);
 
 -- --------------------------------------------------------
 
@@ -96,8 +102,8 @@ INSERT INTO `film_personne` (`ID`, `personne_id`, `film_id`, `role`) VALUES
 
 CREATE TABLE `personnes` (
   `personne_id` int(11) NOT NULL,
-  `nom` varchar(10) DEFAULT NULL,
-  `prenom` varchar(10) DEFAULT NULL,
+  `nom` varchar(30) DEFAULT NULL,
+  `prenom` varchar(30) DEFAULT NULL,
   `date_naissance` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -107,7 +113,18 @@ CREATE TABLE `personnes` (
 
 INSERT INTO `personnes` (`personne_id`, `nom`, `prenom`, `date_naissance`) VALUES
 (1, 'MCGregor', 'Ewan', '1971-03-31'),
-(2, 'Boudhali', 'Tarek', '1979-11-05');
+(2, 'Boudhali', 'Tarek', '1979-11-05'),
+(3, 'BUREAU', 'Bastien', '2000-04-05'),
+(4, 'Johny', 'John', '2000-02-14'),
+(5, 'Johny', 'Johny', '2000-12-25'),
+(6, 'bibi', 'jiji', '1999-01-01'),
+(8, 'DiCaprio', 'Leonardo', '1974-11-11'),
+(9, 'Nolan', 'Christopher', '1970-07-30'),
+(10, 'Pitt', 'Brad', '1963-12-18'),
+(11, 'Fincher', 'David', '1962-08-28'),
+(12, 'McConaughey', 'Matthew', '1969-11-04'),
+(13, 'Johansson', 'Scarlett', '1984-11-22'),
+(14, 'Clooney', 'George', '1961-05-06');
 
 -- --------------------------------------------------------
 
@@ -119,6 +136,16 @@ CREATE TABLE `realisateur` (
   `realisateur_id` int(11) NOT NULL,
   `personne_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `realisateur`
+--
+
+INSERT INTO `realisateur` (`realisateur_id`, `personne_id`) VALUES
+(1, 2),
+(2, 9),
+(3, 11),
+(5, 14);
 
 --
 -- Index pour les tables déchargées
@@ -147,14 +174,6 @@ ALTER TABLE `film`
   ADD KEY `FK_FilmCast` (`cast_id`);
 
 --
--- Index pour la table `film_personne`
---
-ALTER TABLE `film_personne`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `personne_id` (`personne_id`),
-  ADD KEY `film_id` (`film_id`);
-
---
 -- Index pour la table `personnes`
 --
 ALTER TABLE `personnes`
@@ -175,37 +194,31 @@ ALTER TABLE `realisateur`
 -- AUTO_INCREMENT pour la table `acteurs`
 --
 ALTER TABLE `acteurs`
-  MODIFY `acteur_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `acteur_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `cast`
 --
 ALTER TABLE `cast`
-  MODIFY `cast_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cast_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `film`
 --
 ALTER TABLE `film`
-  MODIFY `film_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT pour la table `film_personne`
---
-ALTER TABLE `film_personne`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `film_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `personnes`
 --
 ALTER TABLE `personnes`
-  MODIFY `personne_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `personne_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT pour la table `realisateur`
 --
 ALTER TABLE `realisateur`
-  MODIFY `realisateur_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `realisateur_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Contraintes pour les tables déchargées
@@ -229,13 +242,6 @@ ALTER TABLE `cast`
 --
 ALTER TABLE `film`
   ADD CONSTRAINT `FK_FilmCast` FOREIGN KEY (`cast_id`) REFERENCES `cast` (`cast_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `film_personne`
---
-ALTER TABLE `film_personne`
-  ADD CONSTRAINT `film_personne_ibfk_1` FOREIGN KEY (`personne_id`) REFERENCES `personnes` (`personne_id`),
-  ADD CONSTRAINT `film_personne_ibfk_2` FOREIGN KEY (`film_id`) REFERENCES `film` (`film_id`);
 
 --
 -- Contraintes pour la table `realisateur`
